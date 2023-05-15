02cd215481e59ed75654d47052500720b0887c14
Onur Akdemir <onur.akdemir@icloud.com>
2023-05-15 22:41:36 +0300

    tuple tests updated for floating point equality

--- tests/RTCTuple_test.cpp
+++ tests/RTCTuple_test.cpp
@@ -1,14 +1,15 @@
 #include "RTCTuple.h"
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include <cstdint>
 
 TEST_CASE("Tuple, point tests", "[Tuple]") {
   RTC::Tuple a(4.3, -4.2, 3.1, 1.0);
   RTC::Tuple b = RTC::Point(4.3, -4.2, 3.1);
-  REQUIRE(a.x() == Approx(4.3));
-  REQUIRE(a.y() == Approx(-4.2));
-  REQUIRE(a.z() == Approx(3.1));
-  REQUIRE(a.w() == Approx(1.0));
+  REQUIRE_THAT(a.x(), Catch::Matchers::WithinAbs(4.3, 1e-8));
+  REQUIRE_THAT(a.y(), Catch::Matchers::WithinAbs(-4.2, 1e-8));
+  REQUIRE_THAT(a.z(), Catch::Matchers::WithinAbs(3.1, 1e-8));
+  REQUIRE_THAT(a.w(), Catch::Matchers::WithinAbs(1.0, 1e-8));
   REQUIRE(a.isPoint());
   REQUIRE(!a.isVector());
   REQUIRE(a == b);
@@ -16,10 +17,10 @@ TEST_CASE("Tuple, point tests", "[Tuple]") {
 TEST_CASE("Tuple, vector tests", "[Tuple]") {
   RTC::Tuple a(4.3, -4.2, 3.1, 0.0);
   RTC::Tuple b = RTC::Vector(4.3, -4.2, 3.1);
-  REQUIRE(a.x() == Approx(4.3));
-  REQUIRE(a.y() == Approx(-4.2));
-  REQUIRE(a.z() == Approx(3.1));
-  REQUIRE(a.w() == Approx(1.0));
+  REQUIRE_THAT(a.x(), Catch::Matchers::WithinAbs(4.3, 1e-8));
+  REQUIRE_THAT(a.y(), Catch::Matchers::WithinAbs(-4.2, 1e-8));
+  REQUIRE_THAT(a.z(), Catch::Matchers::WithinAbs(3.1, 1e-8));
+  REQUIRE_THAT(a.w(), Catch::Matchers::WithinAbs(0.0, 1e-8));
   REQUIRE(!a.isPoint());
   REQUIRE(a.isVector());
   REQUIRE(a == b);
