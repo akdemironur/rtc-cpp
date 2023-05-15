2d80cea7e8e9f7e0ee53c543e4bc9a891a4e7a7b
Onur Akdemir <onur.akdemir@icloud.com>
2023-05-15 21:57:50 +0300

    Tuple tests

--- tests/RTCTuple_test.cpp
+++ tests/RTCTuple_test.cpp
@@ -2,9 +2,25 @@
 #include <catch2/catch_test_macros.hpp>
 #include <cstdint>
 
-TEST_CASE("Factorials are computed", "[factorial]") {
-  REQUIRE(factorial(1) == 1);
-  REQUIRE(factorial(2) == 2);
-  REQUIRE(factorial(3) == 6);
-  REQUIRE(factorial(10) == 3'628'800);
+TEST_CASE("Tuple, point tests", "[Tuple]") {
+  RTC::Tuple a(4.3, -4.2, 3.1, 1.0);
+  RTC::Tuple b = RTC::Point(4.3, -4.2, 3.1);
+  REQUIRE(a.x() == Approx(4.3));
+  REQUIRE(a.y() == Approx(-4.2));
+  REQUIRE(a.z() == Approx(3.1));
+  REQUIRE(a.w() == Approx(1.0));
+  REQUIRE(a.isPoint());
+  REQUIRE(!a.isVector());
+  REQUIRE(a == b);
+}
+TEST_CASE("Tuple, vector tests", "[Tuple]") {
+  RTC::Tuple a(4.3, -4.2, 3.1, 0.0);
+  RTC::Tuple b = RTC::Vector(4.3, -4.2, 3.1);
+  REQUIRE(a.x() == Approx(4.3));
+  REQUIRE(a.y() == Approx(-4.2));
+  REQUIRE(a.z() == Approx(3.1));
+  REQUIRE(a.w() == Approx(1.0));
+  REQUIRE(!a.isPoint());
+  REQUIRE(a.isVector());
+  REQUIRE(a == b);
 }
